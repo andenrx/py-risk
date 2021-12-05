5cf9912c2233e2483f7d134dc8b6de6d85974d3a
andenrx <andenrx@gmail.com>
2021-12-05 16:46:27 -0700

    Support Batched MCTS

---
+++ risk/data_loader.py
@@ -0,0 +1,93 @@
+from .orders import *
+try:
+    import torch
+    import torch_geometric
+    from torch_geometric.data import Data
+    from torch_geometric.loader import DataLoader
+
+    class StateData(Data):
+      def __inc__(self, key, value, *args, **kwargs):
+        if 'btch' in key:
+          return self.num_moves
+        elif 'index' in key or 'face' in key or 'src' in key or 'dst' in key or 'tgt' in key:
+          return self.num_nodes
+        else:
+          return 0
+
+      def __cat_dim__(self, key, value, *args, **kwargs):
+        if 'global' in key:
+          return None
+        elif 'index' in key or 'face' in key:
+          return 1
+        else:
+          return 0
+
+except ImportError:
+    pass
+    
+def extract_from_orders(moves, state):
+  asrcs, adsts, abtch, aarmies = [], [], [], []
+  tsrcs, tdsts, tbtch, tarmies = [], [], [], []
+  dtgts, dbtch,        darmies = [], [], []
+  for i, orders in enumerate(moves):
+    for move in orders:
+      if isinstance(move, AttackTransferOrder) and state.owner[move.src] != state.owner[move.dst]:
+        asrcs.append(move.src)
+        adsts.append(move.dst)
+        abtch.append(i)
+        aarmies.append(move.armies)
+      elif isinstance(move, AttackTransferOrder) and state.owner[move.src] == state.owner[move.dst]:
+        tsrcs.append(move.src)
+        tdsts.append(move.dst)
+        tbtch.append(i)
+        tarmies.append(move.armies)
+      elif isinstance(move, DeployOrder):
+        dtgts.append(move.target)
+        dbtch.append(i)
+        darmies.append(move.armies)
+      else:
+        raise Exception(f"Move is of type '{type(move)}'")
+  return {
+      'asrcs':   torch.tensor(asrcs,   dtype=torch.long),
+      'adsts':   torch.tensor(adsts,   dtype=torch.long),
+      'abtch':   torch.tensor(abtch,   dtype=torch.long),
+      'aarmies': torch.tensor(aarmies, dtype=torch.long),
+      'tsrcs':   torch.tensor(tsrcs,   dtype=torch.long),
+      'tdsts':   torch.tensor(tdsts,   dtype=torch.long),
+      'tbtch':   torch.tensor(tbtch,   dtype=torch.long),
+      'tarmies': torch.tensor(tarmies, dtype=torch.long),
+      'dtgts':   torch.tensor(dtgts,   dtype=torch.long),
+      'dbtch':   torch.tensor(dbtch,   dtype=torch.long),
+      'darmies': torch.tensor(darmies, dtype=torch.long),
+  }
+
+def build_order_data(moves, state, x1):
+  data = extract_from_orders(moves, state)
+  bonus_features = torch.tensor([[
+    x1[(x1[:,5+j] == 1) & (torch.arange(20) != i), 0].mean() if x1[i,5+j] == 1 else 0
+    for j in range(10)] for i in range(20)
+  ])
+  
+  data["attack_data"] = torch.cat([
+    x1[data["asrcs"], :],
+    x1[data["adsts"], :],
+    bonus_features[data["asrcs"], :],
+    bonus_features[data["adsts"], :],
+    data["aarmies"].view(-1, 1),
+    (0.6 * data["aarmies"] - 0.7 * (x1[data["adsts"], 3] + x1[data["adsts"], 4])).view(-1, 1)
+  ], dim=1)
+  data["transfer_data"] = torch.cat([
+      x1[data["tsrcs"], :],
+      x1[data["tdsts"], :],
+      bonus_features[data["tsrcs"], :],
+      bonus_features[data["tdsts"], :],
+      data["tarmies"].view(-1, 1),
+  ], dim=1)
+  data["deploy_data"] = torch.cat([
+      x1[data["dtgts"], :],
+      bonus_features[data["dtgts"], :],
+      data["darmies"].view(-1, 1)
+  ], dim=1)
+  del data["aarmies"], data["tarmies"], data["darmies"]
+  return data
+
--- risk/mcts_helper.py
+++ risk/mcts_helper.py
@@ -1,6 +1,7 @@
 from montecarlo.montecarlo import MonteCarlo
 from montecarlo.node import Node
 from .rand import rand_move
+from .data_loader import *
 from time import time
 import math
 
@@ -60,7 +61,7 @@ class MCTS(MonteCarlo):
             child.discovery_factor = self.exploration
             node.add_child(child)
 
-        if self.model and self.model.predict_policy():
+        if self.model and self.model.predict_policy() and not self.model.batched():
             v, pi = self.model(*node.state.to_tensor(player, opponent), [child.move for child in node.children])
             for prior, child in zip(pi.exp().tolist(), node.children):
                 child.update_policy_value(self.trust_policy * prior * len(node.children) + 1 - self.trust_policy)
@@ -72,7 +73,7 @@ class MCTS(MonteCarlo):
         winner = node.state.winner()
         if winner is not None:
             return 1 if winner == self.player else -1
-        elif self.model and not self.model.predict_policy():
+        elif self.model and not self.model.predict_policy() and not self.model.batched():
             value1 =  self.model(*node.state.to_tensor(self.player, self.opponent)).tolist()
             value2 = -self.model(*node.state.to_tensor(self.opponent, self.player)).tolist()
             assert -1 <= value1 <= 1
@@ -80,6 +81,61 @@ class MCTS(MonteCarlo):
             return 0.5 * (value1 + value2)
         elif node.depth >= self.max_depth:
             return 0
+
+    def expand(self, node):
+        if self.model is None or not self.model.batched():
+            return super().expand(node)
+        if not node.children:
+            self.child_finder(node, self)
+        node.expanded = True
+        children = []
+        for child in node.children:
+            win_value = self.node_evaluator(child, self)
+            if win_value is not None:
+                child.update_win_value(win_value)
+            else:
+                self.child_finder(child, self)
+                children.append(child)
+
+        if not children:
+            return
+        data = DataLoader([self.prep(child) for child in children], batch_size=100)
+        assert data
+
+        vs, pis = [], []
+        for batch in data:
+            v, pi = self.model(batch)
+            vs.append(v)
+            pis.append(pi)
+        assert vs
+        assert pis
+        vs = torch.cat(vs, dim=0)
+        pis = torch.cat(pis, dim=0)
+
+        for v, pi, child in zip(vs, pis, children):
+            player = child.player_number
+            opponent = child.opponent_number
+            for prior, grandchild in zip(pi.exp().tolist(), child.children):
+                grandchild.update_policy_value(self.trust_policy * prior * len(child.children) + 1 - self.trust_policy)
+            child.update_win_value(
+                v.item() if player == self.player else -v.item()
+            )
+
+    def prep(self, node):
+        x1, x2, edges = node.state.to_tensor(self.player, self.opponent)
+        edges = torch_geometric.utils.to_undirected(edges)
+        assert torch_geometric.utils.is_undirected(edges)
+
+        orders = [child.move for child in node.children]
+        order_data = build_order_data(orders, node.state, x1)
+        return StateData(
+            num_nodes=len(node.state.mapstruct),
+            num_moves=len(orders),
+            graph_data=x1,
+            global_data=x2,
+            edge_index=edges,
+            **order_data,
+        )
 
     def play(self, mapstate):
         assert mapstate.winner() is None
